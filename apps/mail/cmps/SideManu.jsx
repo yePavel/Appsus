8cf12430a9a77ada63e062dbb7264c86b1c35c8b
pavel <pavel.yv@gmail.com>
2024-05-27 20:47:02 +0300

    Add img display

--- apps/mail/cmps/SideManu.jsx
+++ apps/mail/cmps/SideManu.jsx
@@ -13,32 +13,32 @@ export function SideMenu({ unreadMails, toggleCompose, filterBy, onFilter }) {
 
     return <div className='side-menu-container'>
         <button className='compose-btn' onClick={() => toggleCompose()}>
-            <img className='icon' src="/assets/img/mail-icons/pencil.png" alt="" />
+            <img className='icon' src="./assets/img/mail-icons/pencil.png" alt="" />
             Compose
         </button>
 
         <aside className='side-menu'>
             <p className={`${filterByToEdit.status === 'inbox' ? 'active-label' : ''}`}
                 onClick={() => handleChange('inbox')}>
-                <img className='icon' src="/assets/img/mail-icons/inbox.png" alt="" />
+                <img className='icon' src="./assets/img/mail-icons/inbox.png" alt="" />
                 Inbox
                 {unreadMails > 0 && <span className='unread-counter'>{unreadMails}</span>}
             </p>
             <p>
-                <img className='icon' src="/assets/img/mail-icons/star.png" alt="" />
+                <img className='icon' src="./assets/img/mail-icons/star.png" alt="" />
                 Starred
             </p>
             <p>
-                <img className='icon' src="/assets/img/mail-icons/draft.png" alt="" />
+                <img className='icon' src="./assets/img/mail-icons/draft.png" alt="" />
                 Draft
             </p>
             <p className={`${filterByToEdit.status === 'sent' ? 'active-label' : ''}`}
                 onClick={() => handleChange('sent')}>
-                <img className='icon' src="/assets/img/mail-icons/sent.png" alt="" />
+                <img className='icon' src="./assets/img/mail-icons/sent.png" alt="" />
                 Sent
             </p>
             <p>
-                <img className='icon' src="/assets/img/mail-icons/trash.png" alt="" />
+                <img className='icon' src="./assets/img/mail-icons/trash.png" alt="" />
                 Trash
             </p>
         </aside>
