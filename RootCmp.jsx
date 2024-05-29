eec4a71daa02a7e910dd446a4c4588f15906c666
pavel <pavel.yv@gmail.com>
2024-05-29 19:51:17 +0300

    Fix header bug

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,7 +2,6 @@ const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
 import { AppHeader } from "./cmps/AppHeader.jsx"
-import { About } from "./views/About.jsx"
 import { Home } from "./views/Home.jsx"
 import { MailIndex } from "./apps/mail/views/MailIndex.jsx"
 import { NoteIndex } from "./apps/note/views/NoteIndex.jsx"
@@ -10,11 +9,10 @@ import { NoteIndex } from "./apps/note/views/NoteIndex.jsx"
 import { NoteAdd } from "./apps/note/cmps/NoteAdd.jsx"
 
 
-
-
 export function App() {
     return <Router>
         <section className="app">
+            <AppHeader />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/mail" element={<MailIndex />} />
